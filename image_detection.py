dc0254e7243c51a3b0bacf9018f15a88dff7dba6
matttwh0 <matttran2004@gmail.com>
2026-03-02 14:01:17 -0800

    model created, imported to main

--- image_detection.py
+++ image_detection.py
@@ -2,8 +2,13 @@
 from transformers import pipeline 
 from PIL import Image
 
-def get_image_lavels(image_path):
-    classifier = pipeline("zero-shot-image-classification",
+image_path = "/Users/matthewtran/nxp-agentic-geoguesser/nxp-agentic-geoguesser/images/taj-mahal.jpg"
+
+classifier = pipeline("zero-shot-image-classification",
                           model="openai/clip-vit-base-patch32")
+
+def get_image_labels(image_path):
     image = Image.open(image_path)
-    
+    candidates = ["a famous landmark in India", "a historical monument in Europe","a Middle Eastern mosque", "a Southeast Asian temple", "an American capital building"] 
+    results = classifier(image, candidate_labels = candidates)
+    return [r['label'] for r in results if r['score'] > 0.15]
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+import image_detection
 app = FastAPI()
 
 
