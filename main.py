0d150101de6b4d843b09970ba5f3c2a1a3ba9fc2
Adammac7 <adammac2004@gmail.com>
2026-03-02 13:22:35 -0800

    init

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def main():
-    print("Hello from nxp-agentic-geoguesser!")
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
 
 
-if __name__ == "__main__":
-    main()
+
+
